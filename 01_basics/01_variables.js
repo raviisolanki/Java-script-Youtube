47a63d9f3652ec43a762466fc71fa6f071456a5a
raviisolanki <rs7083979@gmail.com>
2026-06-29 10:57:29 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=12323
+let accountEmail="ravi@gmail.com"
+var accountPassword="12334"
+accountCity="Jaipur"
+
+accountEmail="asds@gmail.com"
+accountPassword="2332"
+accountCity="Karnatak"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
