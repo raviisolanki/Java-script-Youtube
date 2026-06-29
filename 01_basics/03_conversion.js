b68816e4bb0e796506c396849b805538d4ae9d13
raviisolanki <rs7083979@gmail.com>
2026-06-29 11:45:53 +0000

    Added conversion file

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,12 @@
+let score = "R"
+
+console.log(typeof score);
+
+let valueinNumber=Number(score)
+console.log(typeof valueinNumber);
+console.log(valueinNumber)
+//"33"==33
+//"33acs"=NaN 
+//1 =true;0=false;
+ //""=false
+ //"Ravi"=true
