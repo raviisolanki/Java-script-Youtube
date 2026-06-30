3d0439032084d750ef75b94aa9d407607fbe50f6
raviisolanki <rs7083979@gmail.com>
2026-06-30 13:54:15 +0000

    add strings

---
+++ 01_basics/strings.js
@@ -0,0 +1,17 @@
+const name="Ravi";
+const repocount=2;
+
+console.log(`Hello my name is ${name} and my repocount is ${repocount}`);
+
+const newName= new String('Ravi-h-com')
+console.log(newName[2])
+console.log(newName.length);
+console.log(newName.charAt(2));
+console.log(newName.indexOf('i'))
+
+const newString=newName.substring(0,2)
+console.log(newString)
+
+const newStringg="   Solanki.  ";
+console.log(newStringg)
+console.log(newStringg.trim());
